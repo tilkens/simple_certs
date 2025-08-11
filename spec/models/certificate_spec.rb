b7d892d2c2ef14fe80d580fbe160cb507c0d9bc8
tilkens <ben@soona.co>
2025-08-11 17:32:58 -0500

    specs to make sure certificate quantities for certificate sum up to quantity

--- spec/models/certificate_spec.rb
+++ spec/models/certificate_spec.rb
@@ -16,4 +16,36 @@ describe Certificate do
   it 'puts the certificate in the organizations default account' do
     expect(subject.certificate_quantities.map(&:account_id)).to eq([ generator.organization.default_account_id ])
   end
+
+  describe 'certificate quantities conservation' do
+    let(:certificate) { generation.certificate }
+    let!(:certificate_quantity) { certificate.certificate_quantities.first }
+
+    context 'when quantities are conserved' do
+      it 'is valid' do
+        expect(certificate).to be_valid
+      end
+    end
+
+    context 'when quantities are not conserved' do
+      before do
+        certificate_quantity.update!(quantity: 50)
+      end
+
+      it 'is not valid' do
+        expect(certificate).not_to be_valid
+        expect(certificate.errors[:base]).to include('sum of certificate quantities (50) must equal certificate quantity (5)')
+      end
+    end
+
+    context 'when certificate quantities are empty' do
+      before do
+        certificate.certificate_quantities.destroy_all
+      end
+
+      it 'is valid (allows empty state during creation)' do
+        expect(certificate).to be_valid
+      end
+    end
+  end
 end
