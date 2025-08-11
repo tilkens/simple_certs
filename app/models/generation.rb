5a73a677b1df5a931bfd045674c4c3eaadf6cb57
tilkens <ben@soona.co>
2025-08-11 17:11:59 -0500

    validations on generation to ensure continguous generation dates

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -13,6 +13,10 @@ class Generation < ApplicationRecord
   validate :start_date_is_less_than_end_date
   validate :start_date_is_not_in_the_future
   validate :end_date_is_not_in_the_future
+  validate :no_overlapping_dates
+  validate :contiguous_with_existing_generations
+
+  private
 
   def issue_certificate
     self.certificate = Certificate.new(
@@ -36,6 +40,33 @@ class Generation < ApplicationRecord
   def end_date_is_not_in_the_future
     if end_date > Date.today
       errors.add(:base, "end date #{end_date} is in the future")
+    end
+  end
+
+  def no_overlapping_dates
+    overlapping_generation = generator.generations
+      .where.not(id: id)
+      .where("start_date < ? AND end_date > ?", end_date, start_date).exists?
+
+    if overlapping_generation
+      errors.add(:base, "generation dates overlap with existing generation")
+    end
+  end
+
+  def contiguous_with_existing_generations
+    existing_generations = generator.generations.where.not(id: id)
+    return if existing_generations.empty?
+
+    previous_contiguous = existing_generations
+      .where("end_date = ?", start_date - 1.day)
+      .exists?
+
+    next_contiguous = existing_generations
+      .where("start_date = ?", end_date + 1.day)
+      .exists?
+
+    if !previous_contiguous && !next_contiguous
+      errors.add(:base, "generation must be contiguous with an existing generation")
     end
   end
 end
